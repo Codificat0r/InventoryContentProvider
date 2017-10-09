b97f7e5343329210cda654acf416f083d689da13
Carlos Cruz Domínguez <carloscruzdominguez97@gmail.com>
2017-10-09 12:18:49 +0200

    Subo

---
+++ app/src/main/java/com/example/inventory/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.inventory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+ * Esta clase da soporte de código a la activity del login
+ * @author Carlos Cruz Domínguez
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
