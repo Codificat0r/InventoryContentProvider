a7eb874a89a7c219658c3ed10f7e50eb2f2f2a15
Carlos Cruz Domínguez <carloscruzdominguez97@gmail.com>
2017-11-08 14:41:08 +0100

    Subo

--- app/src/test/java/com/example/inventorymaterial/ExampleUnitTest.java
+++ app/src/test/java/com/example/inventorymaterial/ExampleUnitTest.java
@@ -10,8 +10,43 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+    //1. El usuario introduce un usuario/email
+
+    //2. El usuario introduce una contraseña
+
+    //3. El usuario existe en la base de datos (Repository)
+
+    //4. Que la contraseña sea correcta (coincide con la base de datos)
+
     @Test
-    public void addition_isCorrect() throws Exception {
+    public void signIn_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+
+
+
+
+
+    }
+
+    //5. El usuario introduce un usuario
+
+    //6. El usuario introduce una contraseña
+
+    //7. El usuario introduce un email
+
+    //8. La contraseña tiene que ser al menos de longitud 6 caracteres
+
+    //9. El email no existe en la base de datos (Repository)
+
+    //10. El usuario no existe en la base de datos
+
+    //11. Doble comprobación de la contraseña
+
+    @Test
+    public void signUp_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+
+
     }
 }
